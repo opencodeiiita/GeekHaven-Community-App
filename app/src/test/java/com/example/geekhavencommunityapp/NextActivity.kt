5a96662003ba728a52e52759187d92a4a19aac76
TRUST-ME-106 <120783726+TRUST-ME-106@users.noreply.github.com>
2023-12-14 22:59:50 +0530

    Create NextActivity

---
+++ app/src/test/java/com/example/geekhavencommunityapp/NextActivity.kt
@@ -0,0 +1,12 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+
+        // Additional initialization or actions for NextActivity can be done here
+    }
+}
