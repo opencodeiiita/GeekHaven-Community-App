84126ca4adbf4c885f4271651d17797c020a1793
TRUST-ME-106 <120783726+TRUST-ME-106@users.noreply.github.com>
2023-12-15 19:17:26 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/geekhavencommunityapp/MainActivity.kt
+++ app/src/main/java/com/example/geekhavencommunityapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.geekhavencommunityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
