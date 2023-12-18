ca7c50e91eab0933a75141c8d60e138692156987
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2023-12-18 06:57:51 +0530

    commit 3

---
+++ app/src/main/java/com/example/geekhavencommunityapp/activities/loginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.geekhavencommunityapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.geekhavencommunityapp.R
+
+class loginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+
+
+
+    }
+}
