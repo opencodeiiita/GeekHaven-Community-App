8eb332434fc83c511af5d052f29602f4ea04f4d1
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-01-06 10:20:33 +0530

    Revert "New Signin Signup Screen"

--- app/src/main/java/com/example/geekhavencommunityapp/activities/usernameActivity.kt
+++ app/src/main/java/com/example/geekhavencommunityapp/activities/usernameActivity.kt
@@ -2,12 +2,15 @@ package com.example.geekhavencommunityapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geekhavencommunityapp.R
 import com.example.geekhavencommunityapp.UserModel
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -63,7 +66,7 @@ class usernameActivity : AppCompatActivity() {
 
             val intent = Intent(
                 this,
-                BaseHomeActivity:: class.java)
+                emailActivity:: class.java)
 
 
 
