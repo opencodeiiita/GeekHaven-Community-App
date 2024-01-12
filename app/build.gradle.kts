1ef562f63acf168c60ddc695f0f71318aa9c4cb5
Sagnik Mandal <acriticalcynic@outlook.com>
2024-01-12 23:30:57 +0530

    Added Chat Feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.tbuonomo:dotsindicator:5.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
